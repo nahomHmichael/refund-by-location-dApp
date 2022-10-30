639b4aee24a7708f53f27721af9eea51d000d6b6
nahomHmichael <nahomhabtemichael@gmail.com>
2022-10-31 00:46:51 +0300

    contract: add check employee method

--- contracts/refund.sol
+++ contracts/refund.sol
@@ -23,3 +23,14 @@ contract refunderContract{
      
     }
     mapping(address => contract_spec) public contractInfo;
+    
+ function kill() public {
+      if(msg.sender == owner) selfdestruct(payable(owner));
+    }
+
+    function check_existance(address employee) private view returns(bool){
+        for(uint256 i = 0; i < employees.length; i++){
+            if(employees[i] == employee) return true;
+        }
+        return false;
+    }
